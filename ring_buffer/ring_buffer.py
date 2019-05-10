dd4291a6a079ba9dd17b2ab7c02adc74d35c5189
KROHNICK <business.kanekperez@gmail.com>
2019-05-10 09:04:34 -0700

    readme test work

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -13,3 +13,26 @@ class RingBuffer:
             return self.storage[:self.current]
         else:
             return self.storage
+
+
+# buffer = RingBuffer(3)
+
+# buffer.get()   # should return []
+
+# buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
+
+# buffer.get()   # should return ['a', 'b', 'c']
+
+# # 'd' overwrites the oldest value in the ring buffer, which is 'a'
+# buffer.append('d')
+
+# buffer.get()   # should return ['d', 'b', 'c']
+
+# buffer.append('e')
+# buffer.append('f')
+
+# buffer.get()   # should return ['d', 'e', 'f']
+
+# print(buffer.get())
